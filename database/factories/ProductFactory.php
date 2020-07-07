978788fd2b8f5e4173ff2c32b74c7f66fbb5e367
Nizamuddin Sulieman <kuntau17@gmail.com>
2020-07-07 19:02:38 +0800

    update deps

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->unique()->word,
+        'name' => $faker->productName,
         'description' => $faker->sentence,
         'barcode' => $faker->unique()->ean13,
         'price' => $faker->numberBetween(1, 400),
